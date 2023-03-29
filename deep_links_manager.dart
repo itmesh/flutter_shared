3a99f9d8651520abe6f1aeb168ed244b4edd52dc
Anita Sajdak <anita.sajdak123@gmail.com>
2023-03-29 11:58:49 +0200

    Changes in DeepLinksManager

--- deep_links_manager.dart
+++ deep_links_manager.dart
@@ -1,14 +1,25 @@
 import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
-import 'package:flutter/material.dart';
-import 'package:letsdanceshared/letsdanceshared.dart';
+import 'package:rxdart/rxdart.dart';
 
 class DeepLinksManager {
+  String _linkPrefix = '';
+  String _uriPrefix = '';
+  String _androidPackageName = '';
+  String _bundleId = '';
+  String _appStoreId = '';
+
+  final BehaviorSubject<PendingDynamicLinkData?> _deepLinks = BehaviorSubject<PendingDynamicLinkData?>.seeded(null);
+  final BehaviorSubject<String?> _errors = BehaviorSubject<String?>.seeded(null);
+
+  Stream<PendingDynamicLinkData?> get deepLinks$ => _deepLinks.stream;
+  Stream<String?> get errors$ => _errors.stream;
+
   Future<String> getDeepLinks(String link) async {
     final DynamicLinkParameters dynamicLinkParams = DynamicLinkParameters(
-      link: Uri.parse('https://letsdanceapp.pl$link'),
-      uriPrefix: 'https://letsdanceapp.page.link/',
-      androidParameters: const AndroidParameters(packageName: 'com.itmesh.letsdance'),
-      iosParameters: const IOSParameters(bundleId: 'com.letsdance.app', appStoreId: '1615947410'),
+      link: Uri.parse('$_linkPrefix$link'),
+      uriPrefix: _uriPrefix,
+      androidParameters: AndroidParameters(packageName: _androidPackageName),
+      iosParameters: IOSParameters(bundleId: _bundleId, appStoreId: _appStoreId),
     );
 
     final Uri dynamicLink = await FirebaseDynamicLinks.instance.buildLink(dynamicLinkParams);
@@ -16,26 +27,35 @@ class DeepLinksManager {
     return dynamicLink.toString();
   }
 
-  Future<void> initDeepLinks() async {
-    //Deep links
-    WidgetsFlutterBinding.ensureInitialized();
+  Future<void> init({
+    required String linkPrefix,
+    required String uriPrefix,
+    required String androidPackageName,
+    required String bundleId,
+    required String appStoreId,
+  }) async {
+    _linkPrefix = linkPrefix;
+    _uriPrefix = uriPrefix;
+    _androidPackageName = androidPackageName;
+    _bundleId = bundleId;
+    _appStoreId = appStoreId;
 
     FirebaseDynamicLinks.instance.onLink.listen((PendingDynamicLinkData dynamicLinkData) {
-      ldRouter.pushNamed(dynamicLinkData.link.path);
-    }).onError((Object? _) {
-      ldRouter.pushNamed('/');
+      _deepLinks.add(dynamicLinkData);
+      throw Exception('coś poszło nie tak');
+    }).onError((Object? error) {
+      _errors.add(error.toString());
+
       return;
     });
   }
 
-  Future<void> openDeepLink() async {
+  Future<void> openInitialLink() async {
     //Get any initial links
     final PendingDynamicLinkData? initialLink = await FirebaseDynamicLinks.instance.getInitialLink();
 
     if (initialLink != null) {
-      final Uri deepLink = initialLink.link;
-      // Example of using the dynamic link to push the user to a different screen
-      ldRouter.pushNamed(deepLink.path);
+      _deepLinks.add(initialLink);
     }
   }
 }
