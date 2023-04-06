d5e8b37d8ab06420638bff065c20df10157fad3f
Przemysław Kędzierski <przemekkt@gmail.com>
2023-04-06 20:44:11 +0200

    Fix

--- lib/flutter_shared.dart
+++ lib/flutter_shared.dart
@@ -1,3 +1,5 @@
+//! Do not export src here, src is internal, only export public API!
+
 export 'src/extensions/_extensions.dart';
 export 'src/debouncer/_debouncer.dart';
 export 'src/managers/_managers.dart';
--- lib/src/_src.dart
+++ lib/src/_src.dart
@@ -1,5 +1,3 @@
-//! Do not export src here, src is internal, only export public API!
-
 export 'debouncer/_debouncer.dart';
 export 'extensions/_extensions.dart';
 export 'managers/_managers.dart';
