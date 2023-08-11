053f540c8c1881aad342d9a733104aefac9c9b41
Anita Sajdak <anita.sajdak123@gmail.com>
2023-08-11 15:20:49 +0200

    Add import

--- lib/src/widgets/inputs/_inputs.dart
+++ lib/src/widgets/inputs/_inputs.dart
@@ -4,3 +4,4 @@ export 'im_dropdown_input.dart';
 export 'im_number_input.dart';
 export 'im_password_input.dart';
 export 'im_date_time_input/_im_date_time_input.dart';
+export 'im_multi_drop_down.dart';
