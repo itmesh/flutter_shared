edf1b082e04b9fc54510479ecd735751f793993e
sajdakk <anita.sajdak123@gmail.com>
2024-04-12 10:27:24 +0200

    Add itm mixpanel

--- lib/src/utils/_utils.dart
+++ lib/src/utils/_utils.dart
@@ -1,3 +1,4 @@
 export 'logger/_logger.dart';
 export 'focus_node_util.dart';
 export 'im_optional.dart';
+export 'itm_mixpanel/_itm_mixpanel.dart';
---
+++ lib/src/utils/itm_mixpanel/itm_analytics_mixpanel.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:mixpanel_flutter/mixpanel_flutter.dart';
+
+///! --- IMPORTANT ---
+/// Doesn't work on macOS!
+class ItmAnalyticsMixpanel {
+  ItmAnalyticsMixpanel(Mixpanel itmMixpanel) : _mixpanel = itmMixpanel;
+
+  // region Values
+
+  /// The Mixpanel instance.
+  final Mixpanel _mixpanel;
+
+  // endregion
+
+  void identifyUser(String userId) async {
+    _mixpanel.identify(userId);
+  }
+
+  void aliasExistingUser(String userId) async {
+    _mixpanel.alias(userId, await _mixpanel.getDistinctId());
+    _mixpanel.identify(userId);
+  }
+
+  void trackRoute(Route<dynamic> route) {
+    _mixpanel.track(route.settings.name ?? 'Unnamed route');
+  }
+
+  void registerSuperProperties(Map<String, dynamic> superProperties) async {
+    _mixpanel.registerSuperProperties(superProperties);
+  }
+
+  void registerUserProperties(Map<String, dynamic> userProperties) async {
+    // Mixpanel does not distinguish between super and user properties.
+    _mixpanel.registerSuperProperties(userProperties);
+  }
+
+  void track(String event, Map<String, dynamic> properties) async {
+    _mixpanel.track(
+      event,
+      properties: properties,
+    );
+  }
+}
