b8fda26d26206f69434d9d261267146e2d428ac4
lukkam1998 <lukasz.kaminski1998@gmail.com>
2025-01-06 17:04:02 +0100

    Remove mixpanel from pubspec

--- lib/src/utils/itm_mixpanel/itm_mixpanel.dart
+++ lib/src/utils/itm_mixpanel/itm_mixpanel.dart
@@ -1,23 +1,23 @@
-import 'package:mixpanel_flutter/mixpanel_flutter.dart';
+// import 'package:mixpanel_flutter/mixpanel_flutter.dart';
 
-class ItmMixpanel extends Mixpanel {
-  ItmMixpanel(super.token);
+// class ItmMixpanel extends Mixpanel {
+//   ItmMixpanel(super.token);
 
-  /// The EU server URL for Mixpanel.
-  static const String _serverURL = 'https://api-eu.mixpanel.com';
+//   /// The EU server URL for Mixpanel.
+//   static const String _serverURL = 'https://api-eu.mixpanel.com';
 
-  static Future<Mixpanel> init(String token) async {
-    final Mixpanel mixpanel = await Mixpanel.init(
-      token,
-      trackAutomaticEvents: true,
-    );
+//   static Future<Mixpanel> init(String token) async {
+//     final Mixpanel mixpanel = await Mixpanel.init(
+//       token,
+//       trackAutomaticEvents: true,
+//     );
 
-    // !-- IMPORTANT --!
-    // This must NEVER be removed.
-    // It ensures that all data is kept within the EU
-    // and that the tracking is GDPR compliant.
-    mixpanel.setServerURL(_serverURL);
+//     // !-- IMPORTANT --!
+//     // This must NEVER be removed.
+//     // It ensures that all data is kept within the EU
+//     // and that the tracking is GDPR compliant.
+//     mixpanel.setServerURL(_serverURL);
 
-    return mixpanel;
-  }
-}
+//     return mixpanel;
+//   }
+// }
